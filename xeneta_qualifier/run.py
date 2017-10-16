f3d3c05a244522d2d83ed4dd358bbb4b68bb2619
Chris Thurber <skiman6010@users.noreply.github.com>
2017-10-16 16:48:34 -0400

    Added Gradient Boosting Classifier.

--- xeneta_qualifier/run.py
+++ xeneta_qualifier/run.py
@@ -1,5 +1,6 @@
 import csv
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import precision_recall_fscore_support
 import numpy as np
@@ -29,6 +30,12 @@ def runSGD(X_train, y_train):
     sgd = SGDClassifier(max_iter=500, loss='modified_huber', penalty='elasticnet', random_state=42)
     sgd.fit(X_train, y_train)
     return sgd
+    
+# Gradient Boosting Classifier
+def runGBC(X_train, y_train):
+  boost = GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0, criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False, presort='auto')
+  boost.fit(X_train, y_train)
+  return boost
  
 def getScores(clf, X, y):
     predictions = clf.predict(X)
@@ -47,5 +54,7 @@ sgd = runSGD(X_train, y_train)
 sgd_scores = getScores(sgd, X_test, y_test)
 print('SGD Scores: ', sgd_scores)
 
-
+boost = runGBC(X_train, y_train)
+boost_scores = getScores(boost, X_test, y_test)
+print('Gradient Boosting Scores: ', boost_scores)
 
